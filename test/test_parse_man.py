67f57362ffe97a551427253c56c7c79d4c2f044b
Puneetha Pai <puneethapai29@gmail.com>
2020-09-18 20:51:26 +0530

    Add simple fuctions with test to get all man pages URLs

---
+++ test/test_parse_man.py
@@ -0,0 +1,13 @@
+import random
+
+import pytest
+import requests
+
+from src.parse_man_pages import get_man_page_urls, parse_page
+
+
+def test_get_man_page_urls(capsys):
+    all_man_pages = get_man_page_urls()
+    assert len(all_man_pages) == 11267
+    resp = requests.get(random.choice(all_man_pages))
+    assert resp.status_code == 200
