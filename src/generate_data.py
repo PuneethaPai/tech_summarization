efd6b370c271defad577cdce9c8fe9e15d2ca9f1
Puneetha Pai <puneethapai29@gmail.com>
2020-09-20 16:58:32 +0530

    WIP parse documentation page, but taking lot of time

--- src/generate_data.py
+++ src/generate_data.py
@@ -1,7 +1,7 @@
 import re
 import subprocess as sp
 from pathlib import Path
-
+import logging
 import requests
 from bs4 import BeautifulSoup, SoupStrainer
 
@@ -9,12 +9,17 @@ from src.utils import write_csv
 
 BASE_URL = "https://man7.org/linux/man-pages/"
 ALL_MAN_PAGE = "https://man7.org/linux/man-pages/dir_all_by_section.html"
+URL_PATTERN = r"(http|ftp|https)://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?"
 
 
 def parse_page(url: str, filter_tag: str = None) -> BeautifulSoup:
     """Given URL returns Beuatiful Soup object to scrap further"""
-    response = requests.get(url)
-    html = response.text
+    try:
+        response = requests.get(url, timeout=(5, 6))
+        html = response.text
+    except Exception as e:
+        logging.error(f"Request to page {url} failed with error: {e.args}")
+        html = ""
     return BeautifulSoup(html, "lxml", parse_only=SoupStrainer(filter_tag))
 
 
@@ -36,9 +41,9 @@ def get_man_entry(command: str) -> str:
             stdout=sp.PIPE,
             stderr=sp.PIPE,
         )
-        return process.stdout if process.returncode == 0 else process.stderr
+        return process.stdout if process.returncode == 0 else None
     except sp.SubprocessError as e:
-        return str(e)
+        return None
 
 
 def save_man_page_urls(path: str = "data/man_page_urls.csv") -> None:
@@ -60,15 +65,32 @@ def generate_tech_summary_data(path: str = "tldr_repo/pages/") -> None:
     path = Path(path)
     for tldr_path in path.iterdir():
         tldr_pages = list(tldr_path.glob("*.md"))
-        summary_data = [
-            dict(command=page.name.replace(".md", ""), summary=page.read_text())
-            for page in tldr_pages
-        ]
-        for data in summary_data:
-            data["man_entry"] = get_man_entry(data["command"])
+        summary_data = list()
+        for page in tldr_pages:
+            data = dict()
+            command = page.name.replace(".md", "")
+            man_entry = get_man_entry(command)
+            tldr_summary = page.read_text()
+            doc_url = get_doc_url(tldr_summary)
+            doc_text = parse_page(doc_url).text if doc_url else None
+            summary_data.append(
+                dict(
+                    command=command,
+                    doc_url=doc_url,
+                    doc_text=doc_text,
+                    man_entry=man_entry,
+                    tldr_summary=tldr_summary,
+                )
+            )
         write_csv(f"data/summary/{tldr_path.name}.csv", summary_data)
 
 
+def get_doc_url(tldr_summary: str) -> str or None:
+    match = re.search(URL_PATTERN, tldr_summary)
+    return match.group() if match else None
+
+
 if __name__ == "__main__":
-    save_man_page_urls()
-    generate_tech_summary_data()
+    # save_man_page_urls()
+    # generate_tech_summary_data()
+    pass
