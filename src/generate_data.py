ae698d2d36e8ad3ec7f7089cc8d472db7ba290cf
Puneetha Pai <puneethapai29@gmail.com>
2020-09-22 15:46:40 +0530

    Threding processing sucks, single threaded finished in 31 odd seconds

--- src/generate_data.py
+++ src/generate_data.py
@@ -1,7 +1,10 @@
+import logging
 import re
 import subprocess as sp
+from concurrent.futures.process import ProcessPoolExecutor
+from concurrent.futures.thread import ThreadPoolExecutor
 from pathlib import Path
-import logging
+
 import requests
 from bs4 import BeautifulSoup, SoupStrainer
 
@@ -12,7 +15,7 @@ ALL_MAN_PAGE = "https://man7.org/linux/man-pages/dir_all_by_section.html"
 URL_PATTERN = r"(http|ftp|https)://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?"
 
 
-def parse_page(url: str, filter_tag: str = None) -> BeautifulSoup:
+def parse_web_page(url: str, filter_tag: str = None) -> BeautifulSoup:
     """Given URL returns Beuatiful Soup object to scrap further"""
     try:
         response = requests.get(url, timeout=(5, 6))
@@ -25,10 +28,24 @@ def parse_page(url: str, filter_tag: str = None) -> BeautifulSoup:
 
 def get_man_page_urls(url: str = ALL_MAN_PAGE) -> list:
     """Specific method to find all man page html page link"""
-    soup = parse_page(url, filter_tag="a")
+    soup = parse_web_page(url, filter_tag="a")
     hrefs = [link.get("href") for link in soup if link.has_attr("href")]
     match_refs = [re.search(r"man\d/.+.html$", ref) for ref in hrefs]
     return [BASE_URL + ref.group() for ref in match_refs if ref]
+
+
+def save_man_page_urls(path: str = "data/man_page_urls.csv") -> None:
+    urls = get_man_page_urls()
+    write_csv(
+        path,
+        [
+            dict(
+                command=re.search(r"[^/]+$", url).group().split(".")[0],
+                man_page_url=url,
+            )
+            for url in urls
+        ],
+    )
 
 
 def get_man_entry(command: str) -> str:
@@ -46,42 +63,29 @@ def get_man_entry(command: str) -> str:
         return None
 
 
-def save_man_page_urls(path: str = "data/man_page_urls.csv") -> None:
-    urls = get_man_page_urls()
-    write_csv(
-        path,
-        [
-            dict(
-                command=re.search(r"[^/]+$", url).group().split(".")[0],
-                man_page_url=url,
-            )
-            for url in urls
-        ],
-    )
-
-
 def generate_tech_summary_data(path: str = "tldr_repo/pages/") -> None:
     """Extracts command, tldr summary and man page entry"""
+
+    def parse_single_tldr(page: Path) -> dict:
+        command = page.name.replace(".md", "")
+        man_entry = get_man_entry(command)
+        tldr_summary = page.read_text()
+        doc_url = get_doc_url(tldr_summary)
+        # doc_text = parse_page(doc_url).text if doc_url else None
+        return dict(
+            command=command,
+            doc_url=doc_url,
+            # doc_text=doc_text,
+            man_entry=man_entry,
+            tldr_summary=tldr_summary,
+        )
+
     path = Path(path)
     for tldr_path in path.iterdir():
         tldr_pages = list(tldr_path.glob("*.md"))
-        summary_data = list()
-        for page in tldr_pages:
-            data = dict()
-            command = page.name.replace(".md", "")
-            man_entry = get_man_entry(command)
-            tldr_summary = page.read_text()
-            doc_url = get_doc_url(tldr_summary)
-            doc_text = parse_page(doc_url).text if doc_url else None
-            summary_data.append(
-                dict(
-                    command=command,
-                    doc_url=doc_url,
-                    doc_text=doc_text,
-                    man_entry=man_entry,
-                    tldr_summary=tldr_summary,
-                )
-            )
+        # with ThreadPoolExecutor() as executor:
+        # summary_data = list(executor.map(parse_single_tldr, tldr_pages))
+        summary_data = [parse_single_tldr(tldr_page) for tldr_page in tldr_pages]
         write_csv(f"data/summary/{tldr_path.name}.csv", summary_data)
 
 
@@ -92,5 +96,4 @@ def get_doc_url(tldr_summary: str) -> str or None:
 
 if __name__ == "__main__":
     # save_man_page_urls()
-    # generate_tech_summary_data()
-    pass
+    generate_tech_summary_data()
